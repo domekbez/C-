36621e8347253c62fb78038281e7afd2ec28e2ba
domekbez <49716804+domekbez@users.noreply.github.com>
2019-04-17 14:13:35 +0200

    Add files via upload

---
+++ lab1/Class1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class Matrix
+{
+    public int Rows;
+    public int Columns;
+    public Matrix(int r, int c, double[,] tab)
+    {
+        if(tab==null || r<0|| c<0 || r>tab.GetLength(0) || c>tab.GetLength(1))
+        {
+            Rows = -1;
+            Columns = -1;
+            return;
+        }
+        Rows = r;
+        Columns = c;
+    }
+    public abstract double GetValue(int r, int c);
+    public abstract void SetValue(int r, int c, double val);
+
+    public void Print()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+                Console.Write("{0} ", GetValue(i, j));
+            Console.Write("\n");
+        }
+    }
+
+
+}
+public class ArrayMatrix : Matrix
+{
+    public double[,] elems;
+
+    public ArrayMatrix(int r, int c, double[,] tab):base(r,c,tab)
+    {
+        if (tab == null ||  Rows==-1 || Columns==-1)
+        {
+            elems = null;
+            return;
+        }
+
+        elems = new double[r, c];
+        Rows = r;
+        Columns = c;
+        for (int i = 0; i < Rows; i++)
+            for (int j = 0; j < Columns; j++)
+                elems[i, j] = tab[i, j];      
+            
+    }
+    public override double GetValue(int r, int c)
+    {
+        if (r < 0 || c < 0 || r >= elems.GetLength(0) || c >= elems.GetLength(1))
+            return double.MinValue;
+        return elems[r, c];
+    }
+    public override void SetValue(int r, int c, double val)
+    {
+        if (r >= 0 && c >= 0 && r < elems.GetLength(0) && c < elems.GetLength(1))
+            elems[r, c] = val;
+    }
+
+    //public override void Print()
+    //{
+        
+    //    for (int i = 0; i < Rows; i++)
+    //    {
+    //        for (int j = 0; j < Columns; j++)
+    //            Console.Write("{0} ", elems[i, j]);
+    //        Console.Write("\n");
+    //    }
+    //}
+
+
+}
+public class SparseMatrix:Matrix
+{
+    public double[][,] elems;
+    public SparseMatrix(int r, int c, double[,] tab):base(r,c,tab)
+    {
+        int ilezer = 0;
+        int m = 0, n = 0;
+
+        if (tab == null || Rows == -1 || Columns == -1)
+        {
+            elems = null;
+            return;
+        }
+        elems = new double[r][,];
+        Rows = r;
+        Columns = c;
+        for (int i = 0; i < Rows; i++)
+        {
+            ilezer = 0;
+            for(int j=0;j<Columns;j++)
+            {
+                if (tab[i,j] == 0)
+                    ilezer++;
+            }
+            elems[i] = new double[Columns - ilezer,2];
+        }
+        for (int i = 0; i < Rows; i++, m++)
+        {
+            n = 0;
+            for (int j = 0; j < Columns; j++)
+            {
+                if (tab[i, j] != 0)
+                {
+                    elems[m][n,0] = tab[i, j];
+                    elems[m][n, 1] = j;
+                    n++;
+                }
+            }
+        }
+    }
+    public override double GetValue(int r, int c)
+    {
+        if (r < 0 || c < 0 || r >= Rows || c >= Columns)
+            return double.MinValue;
+        for(int i=0;i<elems[r].GetLength(0);i++)
+        {
+            if (elems[r][i, 1] == c)
+                return elems[r][i, 0];
+        }
+        return 0;
+
+         
+    }
+    public override void SetValue(int r, int c, double val)
+    {
+        if (r < 0 || c < 0 || r >= Rows || c >= Columns)
+            return;
+        for (int i = 0; i < elems[r].GetLength(0); i++)
+        {
+            if (elems[r][i, 1] == c)
+                elems[r][i, 0] = val;
+        }
+        
+    }
+
+    //public override void Print()
+    //{
+    //    for (int i = 0; i < Rows; i++)
+    //    {
+    //        for (int j = 0; j < elems[i].GetLength(0); j++)
+    //            Console.Write("{0} ", elems[i][j,0]);
+    //        Console.Write("\n");
+    //    }
+    //}
+
+
+}
+
---
+++ lab1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===== ETAP 1 =====");
+
+            Matrix m1 = new ArrayMatrix(3, 2, new double[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } });
+
+            Console.WriteLine("Macierz M1:");
+            m1.Print();
+
+            Matrix m2 = new ArrayMatrix(3, 2, null);
+
+            Console.WriteLine("Macierz M2 (brak tablicy wartości w konstruktorze): ");
+            Console.WriteLine("Rows: {0}, Columns: {1}    -    Powinno być -1; -1\n", m2.Rows, m2.Columns);
+
+            Matrix m3 = new ArrayMatrix(3, 3, new double[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } });
+
+            Console.WriteLine("Macierz M2 (niezgodne wymiary): ");
+            Console.WriteLine("Rows: {0}, Columns: {1}    -    Powinno być -1; -1\n", m3.Rows, m3.Columns);
+
+            double val = m1.GetValue(0, 1);
+
+            Console.WriteLine("Element [0, 1] z M1:");
+            Console.WriteLine("{0}    -    Powinno być 2\n", val);
+
+            val = m1.GetValue(3, 2);
+
+            Console.WriteLine("Element [3, 2] z M1:");
+            Console.WriteLine("{0}    -    Powinno być {1}\n", val, double.MinValue);
+
+            m1.SetValue(2, 1, 100);
+
+            Console.WriteLine("Macierz M1 po zmianie wartości:");
+            m1.Print();
+
+            m1.SetValue(-1, 0, 99.99);
+
+            Console.WriteLine("Macierz M1 po zmianie wartości poza macierzą (Powinna być identyczna jak wyżej):");
+            m1.Print();
+
+            Console.WriteLine("===== ETAP 2 =====");
+
+            Matrix m4 = new SparseMatrix(5, 3, new double[,] { { 0, 2, 0 }, { 3, 0, 0 }, { 0, 6, 0 }, { 2, 3, 0 }, { 0, 0, 0 } });
+
+            Console.WriteLine("Macierz M4:");
+            m4.Print();
+
+            Matrix m5 = new SparseMatrix(3, 2, null);
+
+            Console.WriteLine("Macierz M5 (brak tablicy wartości w konstruktorze): ");
+            Console.WriteLine("Rows: {0}, Columns: {1}    -    Powinno być -1; -1\n", m5.Rows, m5.Columns);
+
+            Matrix m6 = new SparseMatrix(3, 3, new double[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } });
+
+            Console.WriteLine("Macierz M6 (niezgodne wymiary): ");
+            Console.WriteLine("Rows: {0}, Columns: {1}    -    Powinno być -1; -1\n", m3.Rows, m3.Columns);
+
+            val = m4.GetValue(0, 1);
+
+            Console.WriteLine("Element [0, 1] z M4:");
+            Console.WriteLine("{0}    -    Powinno być 2\n", val);
+
+            val = m4.GetValue(2, 2);
+
+            Console.WriteLine("Element [2, 2] z M4:");
+            Console.WriteLine("{0}    -    Powinno być 0\n", val);
+
+            val = m4.GetValue(-2, 0);
+
+            Console.WriteLine("Element poza macierzą z M4:");
+            Console.WriteLine("{0}    -    Powinno być {1}\n", val, double.MinValue);
+
+            Console.WriteLine("===== ETAP 3 =====");
+
+            m4.SetValue(2, 1, 100);
+
+            Console.WriteLine("Macierz M4 po zmianie wartości:");
+            m4.Print();
+
+            m4.SetValue(-1, 0, 99.99);
+
+            Console.WriteLine("Macierz M4 po zmianie wartości poza macierzą (Powinna być identyczna jak wyżej):");
+            m4.Print();
+
+            m4.SetValue(0, 2, 99.99);
+
+            Console.WriteLine("Macierz M4 po zmianie wartości z 0 na niezerową (Powinna być identyczna jak wyżej):");
+            m4.Print();
+
+
+
+
+
+
+
+        }
+    }
+}
